bf05be646529a7b015e48f8ecf69618d8c6ba2f7
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-03-18 15:16:35 +0530

    18/03/26

---
+++ JAVASCRIPT/Video 55/script.js
@@ -0,0 +1,18 @@
+console.log("Hello i am conditional tutorial");
+let a = 45;
+let grace = 3;
+if (a + grace > 18) {
+    console.log("You can drive");
+} else {
+    console.log("You can't drive");
+
+}
+
+let b = 23;
+let c = 45;
+console.log(b + c);
+console.log(b - c);
+console.log(b * c);
+console.log(b / c);
+console.log(b % c);
+console.log(b ** c);
