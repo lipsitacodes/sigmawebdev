ac8dba07db1466579c89d2524470c4ff4fd7638f
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-03-18 11:58:21 +0530

    18/03/26

---
+++ JAVASCRIPT/Video 54/Script.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+console.log("Code is running...")
+
+var a = prompt("Enter your number")
+var istrue = confirm("Are you sure you want to leave this page and blast your computer")
+console.log("Your number is " + a)
+if (istrue) {
+    console.log("Computer is blasting")
+} else {
+    console.log("Computer is not blasting")
+
+}
---
+++ JAVASCRIPT/Video 55 Variable and datatype/script.js
@@ -0,0 +1,22 @@
+console.log("Hey this is turorial 55 of js")
+let a = 5;
+// a = a + 1;
+var b = 6;
+var c = "Lipsita"
+
+// for modern java script always use let instead of var
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c)
+
+{
+    // let locally scope
+    let a = 7;
+    console.log(a)
+    // var is globally scope
+    var b = 76;
+    console.log(b)
+}
+console.log(b)
+console.log(a)
+// const a1 = 8;
+// a1 = a1+1; this is not allowed because this is constant and you can't change the value of constant
