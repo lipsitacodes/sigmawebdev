6457b7c496fd97acb3ae636873c8d58acf6f91a7
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-03-24 19:07:57 +0530

    24/03/26

---
+++ JAVASCRIPT/Video 60 Strings/index.js
@@ -0,0 +1,27 @@
+console.log("tThis is String tutorial");
+let a = "Lipsita";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+// console.log(a[7]);
+// console.log(a[9]);
+
+console.log(a.length);
+let Mname = "Lipsita";
+let friend = "Mamali";
+console.log("Her name is " + Mname + " and her friend name is " + friend);
+
+console.log(`Her name is ${Mname} and her friend name is ${friend}`);
+let b = "Hello World!!"
+console.log(b.toUpperCase(),b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,4));
+console.log(b.replace("ll","45"));
+
+let x = "Lipsita ";
+let y = "Khadgarai";
+console.log(x.concat(y," PKD"));
