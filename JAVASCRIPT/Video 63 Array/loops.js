d7a957f6bb2c1055bbc6b15d749342c0b9b810a8
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-05-02 21:16:33 +0530

    02/05/26

--- JAVASCRIPT/Video 63 Array/loops.js
+++ JAVASCRIPT/Video 63 Array/loops.js
@@ -30,13 +30,33 @@ for (const element of a) {
 
 
 let arr = [1, 2, 3, 4, 5];
-let newArr = [];
-for(let i = 0; i <arr.length;i++){
-    const element = arr[i];
-    newArr.push(element**2);
+// let newArr = [];
+// for(let i = 0; i <arr.length;i++){
+//     const element = arr[i];
+//     newArr.push(element**2);
 
-}
+// }
+let newArr = arr.map((e, i, arr) => {
+    console.log("Element:", e, "| Index:", i, "| Full Array:", arr);
+    return e ** 2;
+})
 console.log("Printing all the values of old array:");
 console.log(arr);
 console.log("Printing all the values of new array by squaring the values of the old array:");
 console.log(newArr);
+
+const greaterthanseven = (e) => {
+    if (e > 7) {
+
+        return true;
+    }
+    return false;
+}
+console.log(a.filter(greaterthanseven));
+console.log("Printing all the values of array that are greater than 7:");
+
+let arr2 = [3,4,6,5,2,1];
+const red = (a,b) =>{
+    return a*b;
+}
+console.log(arr2.reduce(red)); //3*4*6*5*2*1 = 720
