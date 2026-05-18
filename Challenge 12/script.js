4176de968221c25cdd017cefaa0a088376fd0e28
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-05-18 15:46:13 +0530

    18 may

--- Challenge 12/script.js
+++ Challenge 12/script.js
@@ -1,25 +1,39 @@
-const e = document.getElementsByTagName("div");
-console.log(e);
+// const e = document.querySelectorAll(".box");
+// console.log(e);
 
-for(let i = 0; i < e.length; i++) {
+// for(let i = 0; i < e.length; i++) {
 
-    let random = Math.random();
-    console.log(random);
-    if (random <= 0.20) {
-        e[i].style.backgroundColor = "red";
+//     let random = Math.random();
+//     console.log(random);
+//     if (random <= 0.20) {
+//         e[i].style.backgroundColor = "red";
 
-    } else if (random <= 0.40) {
-        e[i].style.backgroundColor = "blue";
+//     } else if (random <= 0.40) {
+//         e[i].style.backgroundColor = "blue";
 
-    } else if (random <= 0.60) {
-        e[i].style.backgroundColor = "green";
+//     } else if (random <= 0.60) {
+//         e[i].style.backgroundColor = "green";
 
-    } else if (random <= 0.80) {
-        e[i].style.backgroundColor = "yellow";
+//     } else if (random <= 0.80) {
+//         e[i].style.backgroundColor = "yellow";
 
-    } else {
-        e[i].style.backgroundColor = "purple";
-    }
-}
+//     } else {
+//         e[i].style.backgroundColor = "purple";
+//     }
+// }
 
-// when i am runnig the code in console it's actually workig but in editor it's showing undefined style property
+// // when i am runnig the code in console it's actually workig but in editor it's showing undefined style property
+
+const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+const textColors = ["white", "black"];
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => {
+
+    let bg = colors[Math.floor(Math.random() * colors.length)];
+    let text = textColors[Math.floor(Math.random() * textColors.length)];
+
+    box.style.backgroundColor = bg;
+    box.style.color = text;
+});
