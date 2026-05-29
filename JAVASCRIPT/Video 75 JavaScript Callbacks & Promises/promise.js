226337c9571e5586aa4f23804772b49a93be009b
Lipsita <lipsitakhadgarai16@gmail.com>
2026-05-29 19:20:50 +0530

    Promise compelted 29/05/26

--- JAVASCRIPT/Video 75 JavaScript Callbacks & Promises/promise.js
+++ JAVASCRIPT/Video 75 JavaScript Callbacks & Promises/promise.js
@@ -6,17 +6,37 @@ let prom1 = new Promise((resolve, reject) => {
     }
     else{
         setTimeout(() => {
-            console.log("Yes i am done");
-            resolve("Lipsita")
+            console.log("Yes i am done 1");
+            resolve("Lipsita1")
         }, 3000)
     }   
 })
-prom1.then((a) => {
-    console.log(a);
-}).catch((error) => {   
-    console.log("My site is under maintenance");
-}).finally(()=>{
-    console.log("I am finally done");
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if(a<0.5){
+        reject("Sorry random number is not supporting");
+    }
+    else{
+        setTimeout(() => {
+            console.log("Yes i am done 2");
+            resolve("Lipsita2")
+        }, 3000)
+    }   
 })
+// prom1.then((a) => {
+//     console.log(a);
+// }).catch((error) => {   
+//     console.log("My site is under maintenance");
+// }).finally(()=>{
+//     console.log("I am finally done");
+// })
+
+let p3 = Promise.any([prom1, prom2]);    
+p3.then((a) => {
+  console.log(a);
+}).catch((e) => {
+  console.log(e);
+}
+)
 
 
