ca0d2ad057ed911665a9b3904004096b449c96b7
Lipsita <lipsitakhadgarai16@gmail.com>
2026-05-29 19:01:00 +0530

    Promise compelted 29/05/26

--- JAVASCRIPT/Video 75 JavaScript Callbacks & Promises/promise.js
+++ JAVASCRIPT/Video 75 JavaScript Callbacks & Promises/promise.js
@@ -1,12 +1,22 @@
 console.log("This is Promises");
-
 let prom1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Yes i am done");
-        resolve("Lipsita")
-    }, 3000)
-
+    let a = Math.random();
+    if(a<0.5){
+        reject("Sorry random number is not supporting");
+    }
+    else{
+        setTimeout(() => {
+            console.log("Yes i am done");
+            resolve("Lipsita")
+        }, 3000)
+    }   
 })
 prom1.then((a) => {
     console.log(a);
+}).catch((error) => {   
+    console.log("My site is under maintenance");
+}).finally(()=>{
+    console.log("I am finally done");
 })
+
+
